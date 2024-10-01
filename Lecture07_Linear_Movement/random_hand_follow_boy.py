c2ca0408fb8fb981299f2875178fa1698eb0489e
minseokcha@laptop <alstjr0526@gmail.com>
2024-10-01 16:19:49 +0900

    기초작업
    배경, 애니, 손, 로드 후 그리기 성공

--- Lecture07_Linear_Movement/random_hand_follow_boy.py
+++ Lecture07_Linear_Movement/random_hand_follow_boy.py
@@ -1,1 +1,31 @@
 from pico2d import*
+
+TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
+tuk_ground = load_image('TUK_GROUND.png')
+hand = load_image('hand_arrow.png')
+character = load_image('animation_sheet.png')
+
+
+alive =True
+
+while alive:
+
+    clear_canvas()
+    tuk_ground.draw(TUK_WIDTH//2, TUK_HEIGHT//2)
+    character.draw(500, 500)
+    hand.draw(400,400)
+
+    
+    update_canvas()
+    handle_events()
+
+
+    delay(2)
+
+
+
+
+
+close_canvas()
